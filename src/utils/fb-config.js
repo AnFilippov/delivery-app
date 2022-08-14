47c4a968064adfc2f32d2fa6774a5af9ed29fca7
andrey <quesandans491@gmail.com>
2022-08-14 16:49:34 +0300

    upgrade database

--- src/utils/fb-config.js
+++ src/utils/fb-config.js
@@ -3,13 +3,13 @@ import 'firebase/auth'
 import 'firebase/database'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB4mfB0woMSY9X-dgez4-_mO7G9O7qI9Oo",
-  authDomain: "delivery-app-c0a0b.firebaseapp.com",
-  databaseURL: "https://delivery-app-c0a0b-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "delivery-app-c0a0b",
-  storageBucket: "delivery-app-c0a0b.appspot.com",
-  messagingSenderId: "513920629259",
-  appId: "1:513920629259:web:a5142e15da01b89dbb650e"
+  apiKey: "AIzaSyAp6i87WcBXkYLxg3j5LB1QAsTgheogSso",
+  authDomain: "delivery-app-18232.firebaseapp.com",
+  databaseURL: "https://delivery-app-18232-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "delivery-app-18232",
+  storageBucket: "delivery-app-18232.appspot.com",
+  messagingSenderId: "162616473947",
+  appId: "1:162616473947:web:1053b3ce13cc3c89d4827e"
 };
 
 firebase.initializeApp(firebaseConfig);
